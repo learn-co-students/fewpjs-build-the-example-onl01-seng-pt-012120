bceff0843b48ff6fc57ac48c9a7f917842cd632a
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-07-27 17:21:42 +0000

    Done.

--- main.js
+++ main.js
@@ -5,11 +5,38 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 const modal = document.getElementById('modal');
+const likeButtons = document.querySelectorAll('.like-glyph');
 modal.setAttribute("class", "hidden");
+document.addEventListener('DOMContentLoaded', () => {
+  const likeButtons = document.querySelectorAll('.like-glyph');
+  const modal = document.getElementById('modal');
 
+  function displayError(error) {
+    modal.removeAttribute('class');
+    modal.textContent = error;
+    setTimeout(function(){ modal.setAttribute('class','hidden') }, 5000);
+  };
 
+  function likeBtn(button) {
+    button.addEventListener('click', (event) => {
+      if (event.target.innerHTML === EMPTY_HEART) {
+        mimicServerCall().then(() => {
+            event.target.innerHTML = FULL_HEART;
+            event.target.setAttribute('class', 'like-glyph activated-heart');
+        }).catch((error) => {
+            displayError(error);
+        });
+      } else {
+        event.target.innerHTML = EMPTY_HEART
+        event.target.setAttribute('class', 'like-glyph')
+      }
+    });
+  };
 
-
+  likeButtons.forEach(button => {
+    likeBtn(button)
+  });
+});
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
