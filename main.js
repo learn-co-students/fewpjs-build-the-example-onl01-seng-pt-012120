0bc94a1c5cbf97437747512c53b27f53ace73b6e
hornadam8 <hornadam8@gmail.com>
2020-08-01 16:01:31 -0600

    Done.

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const likeGlyphs = document.querySelectorAll('.like-glyph')
+const error = document.getElementById('modal')
 
 // Your JavaScript code goes here!
+error.classList.add('hidden')
 
+function likeCallback(e) {
+  let heart = e.target
+  mimicServerCall()
+    .then(function(serverMessage){
+      alert(serverMessage);
+      if (heart.innerText === EMPTY_HEART){
+        heart.innerText = FULL_HEART
+        heart.classList.add('activated-heart')
+      }
+      else {
+        heart.innerText = EMPTY_HEART
+        heart.classList.remove('activated-heart')
+      }
+    })
+    .catch(function(error) {
+      alert("Something went wrong!");
+      error.classList.remove('hidden')
+    });
+}
 
+for (let glyph of likeGlyphs){
+  glyph.addEventListener("click",likeCallback)
+}
 
 
 //------------------------------------------------------------------------------
@@ -18,7 +43,7 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
-        resolve("Pretend remote server notified of action!");
+        resolve();
       }
     }, 300);
   });
